d9a868b0a77aa414c0d5e058c29e047fb4ecef7d
dhanaprakash858 <dhanaprakash.858@gmail.com>
2023-03-11 23:47:01 +0530

    alphacare

---
+++ src/org/git/Class.java
@@ -0,0 +1,11 @@
+package org.git;
+
+public class Class {
+
+	public static void main(String[] args) {
+		System.out.println("prakash");
+		System.out.println("prakash");
+		System.out.println("prakash");
+		System.out.println("prakash");
+	}
+}
